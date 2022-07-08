b4fe9e7d5b448e95329c925c8dcb7cc410774d1f
victorhsb <victorhsbarrozo@gmail.com>
2022-07-08 00:15:02 -0300

    renamed storage to repository and init functions

--- repository/interface.go
+++ repository/interface.go
@@ -1,13 +1,13 @@
-package storage
+package repository
 
 type Interface interface {
 	Read(path string) (*Route, error)
 	List(prefix string) ([]string, error)
 }
 
-// NewStorage gets a source and returns an Interface based on the type of source
+// New gets a source and returns an Interface based on the type of source
 // by now the only available input is file storage (starting with '/')
-func NewStorage(source string) Interface {
+func New(source string) Interface {
 	if source[0] == '/' { // file storage
 		return &FileStorage{root: source}
 	}
--- service/routes.go
+++ service/routes.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 
 	"github.com/victorhsb/go-mocker/model"
-	"github.com/victorhsb/go-mocker/storage"
+	"github.com/victorhsb/go-mocker/repository"
 )
 
 type MockSvc struct {
-	storage storage.Interface
+	storage repository.Interface
 }
 
-func NewService(storage storage.Interface) *MockSvc {
+func New(storage repository.Interface) *MockSvc {
 	return &MockSvc{storage: storage}
 }
 
@@ -25,8 +25,8 @@ func (s *MockSvc) GetRoutes() ([]*model.Route, error) {
 	for _, f := range files {
 		r, err := s.storage.Read(f)
 		if err != nil {
-			if err == storage.ErrFormatNotSuported { // skip unsupported files
-				fmt.Printf("skipping file %w as it's extension is not supported\n", f)
+			if err == repository.ErrFormatNotSuported { // skip unsupported files
+				fmt.Printf("skipping file %s as it's extension is not supported\n", f)
 				continue
 			}
 			return nil, fmt.Errorf("could not read route %s; %w", f, err)
